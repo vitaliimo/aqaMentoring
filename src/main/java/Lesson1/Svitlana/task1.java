a0c9142769e5c12d796fcc33de23a71c208bb5f9
SvitlanaBychevska <Svitlana_Bychevska@epam.com>
2020-09-23 11:19:13 +0300

    Home work

---
+++ src/main/java/Lesson1/Svitlana/task1.java
@@ -0,0 +1,11 @@
+package Lesson1.Svitlana;
+
+public class task1 {
+    public static void main(String[] args) {
+    int a1 = 74;
+    int a2 = 36;
+    int a3;
+    a3 = a1 + a2;
+    System.out.println(a3);
+    }
+}
