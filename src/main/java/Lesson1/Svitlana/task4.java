3b5fb8428aea23608ace6dcaf0fe1e493c1b1129
Vitalii_Mochkodan <Vitalii.Mochkodan@McKesson.com>
2020-10-04 16:06:24 +0300

    Lesson2

--- src/main/java/Lesson1/Svitlana/task4.java
+++ src/main/java/Lesson1/Svitlana/task4.java
@@ -7,7 +7,7 @@ public class task4 {
         int n=7;
         int a;
         int b;
-
+//initialize every variable
     int[] mass = new int[n];
     mass[0] = 5;
     mass[1] = 6;
@@ -16,10 +16,11 @@ public class task4 {
     mass [4] = -5;
     mass [5] = 32;
     mass [6] = 43;
-    b = 0;
+    b = 0; // b is useless
     for (int i = 0; i < n; i++) {
          a = b + mass[i];
          b = a;
+         //a = a + mas[i]
         }
 System.out.println(b);
     }
---
+++ src/main/java/Lesson2/Poly.java
@@ -0,0 +1,51 @@
+package Lesson2;
+
+public class Poly {
+
+    public static void main(String[] args) {
+        Tenis tenis = new Tenis();
+        TableTennis tenis1 = new TableTennis();
+
+        tenis.play("d","d");
+
+        tenis1.play();
+        tenis1.play("d","d");
+    }
+
+
+    static class Tenis {
+        String player1;
+        String player2;
+
+        public Tenis(String p1, String p2) {
+            System.out.println("instanse is created");
+        }
+
+        public Tenis() {
+            System.out.println("Tenis instanse is created");
+        }
+
+        public void play (String p1, String p2){
+            System.out.println(p1 + p2 + "Game started");
+        }
+    }
+
+    static class TableTennis extends Tenis{
+
+        public TableTennis() {
+            System.out.println("TableTennis instanse is created");
+        }
+
+        //перегружен
+        public void play() {
+            System.out.println("table tennis is better");
+        }
+
+        //переопределен
+        @Override
+        public void play (String p1, String p2){
+            System.out.println(p1 + p2 + "  Table tennis ame started Override");
+        }
+    }
+
+}
