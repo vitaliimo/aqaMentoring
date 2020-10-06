ca2da5059b5702e34a8c9a820290d2c5d2982431
SvitlanaBychevska <Svitlana_Bychevska@epam.com>
2020-10-06 23:52:16 +0300

    Home work_task 7

--- src/main/java/Lesson2/Svitlana/Shape.java
+++ src/main/java/Lesson2/Svitlana/Shape.java
@@ -3,21 +3,62 @@ package Lesson2.Svitlana;
 import java.util.Scanner;
 
 public class Shape {
-    public double volume;
+    public static double volume;
 
     public static void main(String[] args) {
         Shape newShape = new Shape();
-        newShape.setVolume();
-        newShape.getVolume();
-    }
-
-    public void setVolume() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input volume");
         volume = scanner.nextDouble();
+        System.out.println("Volume of the shape = " + newShape.getVolume());
     }
 
-    public void getVolume() {
-        System.out.println("Volume of the shape = " + volume);
+    public double getVolume() {
+        return volume;
+    }
+}
+class SolidOfRevolution extends Shape {
+    private static double radius;
+
+    public SolidOfRevolution(){
+        radius = 10;
+    }
+
+    public static void main(String[] args) {
+        SolidOfRevolution newSolidOfRevolution = new SolidOfRevolution();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input Radius");
+        radius = scanner.nextDouble();
+        System.out.println("Volume of the Radius = " + newSolidOfRevolution.getRadius());
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
+class Pyramid extends Shape {
+    private static double s;
+    private static double h;
+
+    public double getH() {
+        return h;
+    }
+
+    public double getS() {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Pyramid newPyramid = new Pyramid();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input h value");
+        System.out.println("h = ");
+        h = scanner.nextDouble();
+        System.out.println(newPyramid.getH());
+
+        System.out.println("Please input s value");
+        System.out.println("s = ");
+        s = scanner.nextDouble();
+        System.out.println(newPyramid.getS());
     }
 }
