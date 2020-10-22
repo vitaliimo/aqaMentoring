c311311d5376c036bed87da7fd9b1cfc4d279795
Daryna Horobei <daryna_horobei@epam.com>
2020-10-22 18:56:47 +0300

    add fixed Task 0-5 HW3

--- src/main/java/lesson3/Daryna/Task5.java
+++ src/main/java/lesson3/Daryna/Task5.java
@@ -1,67 +1,47 @@
 package lesson3.Daryna;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Task5 {
-    public static <K, V> Map<K, V> inMapTransformtion2(ArrayList<K> arrayList_K, ArrayList<V> arrayList_V) throws Exception {
-
+    public static <K, V> Map<K, V> inMapTransformtion2(List<K> arrayList_K, List<V> arrayList_V) throws Exception {
         Map<K, V> map = new TreeMap<>();
-
-
         if (arrayList_K.size() == arrayList_V.size()) {
-
             for (int i = 0, j = 0; i < arrayList_K.size(); i++, j++) {
                 map.put(arrayList_K.get(i), arrayList_V.get(j));
             }
-
 
         } else if (arrayList_K.size() > arrayList_V.size())
             throw new Exception("Value is not enough for map");
         else throw new Exception("Key is not enough for map");
-
         return map;
     }
 
 
-    public static <K, V> Map<K, V> inMapTransformtion1(ArrayList<K> arrayList_K, ArrayList<V> arrayList_V) {
-
+    public static <K, V> Map<K, V> inMapTransformtion1(List<K> arrayList_K, List<V> arrayList_V) {
         Map<K, V> map = new TreeMap<>();
         if (arrayList_K.size() == arrayList_V.size()) {
-            for (int i = 0, j = 0; i < arrayList_K.size(); i++, j++) {
-                map.put(arrayList_K.get(i), arrayList_V.get(j));
+            for (int i = 0; i < arrayList_K.size(); i++) {
+                map.put(arrayList_K.get(i), arrayList_V.get(i));
             }
-
         } else if (arrayList_K.size() > arrayList_V.size()) {
-
             int delta = arrayList_K.size() - arrayList_V.size();
-
             for (int i = 1; i <= delta; i++) {
                 arrayList_V.add(null);
             }
-            for (int i = 0, j = 0; i < arrayList_K.size(); i++, j++) {
-                map.put(arrayList_K.get(i), arrayList_V.get(j));
+            for (int i = 0; i < arrayList_K.size(); i++) {
+                map.put(arrayList_K.get(i), arrayList_V.get(i));
             }
-
         } else {
             int delta = arrayList_V.size() - arrayList_K.size();
             for (int i = 1; i <= delta; i++) {
                 arrayList_V.remove(i);
             }
-            for (int i = 0, j = 0; i < arrayList_K.size(); i++, j++) {
-                map.put(arrayList_K.get(i), arrayList_V.get(j));
+            for (int i = 0; i < arrayList_K.size(); i++) {
+                map.put(arrayList_K.get(i), arrayList_V.get(i));
             }
-
-
         }
-
         return map;
-
-
     }
-
 
     public static void main(String[] args) throws Exception {
 
@@ -74,13 +54,21 @@ public class Task5 {
         arrayList_V.add(5);
         arrayList_V.add(3);
         arrayList_V.add(2);
-        arrayList_V.add(4);
 
-        Map<String, Integer> map = inMapTransformtion1(arrayList_K, arrayList_V);
-        System.out.println(map);
 
-//        Map<String, Integer> map1 = inMapTransformtion2(arrayList_K, arrayList_V);
-//        System.out.println(map1);
+        try {
+            Map<String, Integer> map = inMapTransformtion1(arrayList_K, arrayList_V);
+            System.out.println(map);
+        } catch (Exception e) {
+            System.out.println("Value is not enough for map");
+        }
+
+        try {
+            Map<String, Integer> map1 = inMapTransformtion2(arrayList_K, arrayList_V);
+            System.out.println(map1);
+        } catch (Exception e) {
+            System.out.println("Key is not enough for map");
+        }
     }
 
 
